223d8bfd038d5801b32d99a770d0f3097a3a057b
Johnny Cash <ionutnichita@gmail.com>
2020-01-22 22:28:02 +0200

    Added custom endpoint controller and configuration

---
+++ src/Controller/ApiResponseController.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Component\DependencyInjection\ParameterBag\ParameterBagInterface;
+use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\Routing\Annotation\Route;
+use ApiPlatform\Core\Annotation\ApiResource;
+
+class ApiResponseController
+{
+    private $params;
+
+    public function __construct(ParameterBagInterface $params)
+    {
+        $this->params = $params;
+    }
+
+    /**
+     * @Route(
+     *     name="potato sales",
+     *     path="/api/potatoSales",
+     *     methods={"GET"},
+     * )
+     */
+    public function __invoke(): object
+    {
+        $rootDir = $this->params->get('kernel.project_dir');
+        $responseJson = file_get_contents($rootDir . '/staticres/potato_sales.json');
+
+        return new JsonResponse(json_decode($responseJson, true));
+    }
+}

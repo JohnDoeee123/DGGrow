db7947cc879edf8af9b691f2e168d805beeb1825
Johnny Cash <ionutnichita@gmail.com>
2020-01-26 17:31:05 +0200

    Convert custom js into a module to avoid name collisions

--- templates/js/potatoapp.js
+++ templates/js/potatoapp.js
@@ -1,137 +1,139 @@
-let responseData;
-let leaves = [];
+let potatoAppModule = (() => {
+    let responseData;
+    let leaves = [];
 
-function setAddNewProductBehavior() {
-    //add new product behavior
-    $("#newProduct").submit(function (event) {
-        event.preventDefault();
+    function setAddNewProductBehavior() {
+        //add new product behavior
+        $("#newProduct").submit(function (event) {
+            event.preventDefault();
 
-        let formData = {
-            'productName': $('#productName').val(),
-            'productId': $('#productID').val(),
-            'productManager': $('#productManager').val(),
-            'salesStartDate': $('#salesStartDate').val()
-        };
+            let formData = {
+                'productName': $('#productName').val(),
+                'productId': $('#productID').val(),
+                'productManager': $('#productManager').val(),
+                'salesStartDate': $('#salesStartDate').val()
+            };
 
-        makeApiCall('/api/product', formData)
-            .done(function (data) {
-                $('#newProduct')[0].reset();
-                showToast("Server Response", data.result);
-            });
-    });
-
-    $('#resetNewProductForm').click(function () {
-        $('#newProduct')[0].reset();
-    });
-}
-
-function setSalesReportsBehavior() {
-    //display data grid behavior
-    makeApiCall('/api/sales/potato', {}, 'GET')
-        .done(function (data) {
-            console.log("Server Response", data);
-            let gridHtml = generateGrid(data);
-            console.log('grid: ', gridHtml);
-
-            $('#saleReports').html(gridHtml);
-            $('#myTable').DataTable();
+            makeApiCall('/api/product', formData)
+                .done(function (data) {
+                    $('#newProduct')[0].reset();
+                    showToast("Server Response", data.result);
+                });
         });
-}
 
-function showToast(headerTxt, bodyTxt) {
-    $('.toast-body').text(bodyTxt);
-    $('.toast-header').text(headerTxt);
-    $('.toast').toast({delay: 5000});
-    $('.toast').toast('show');
-}
+        $('#resetNewProductForm').click(function () {
+            $('#newProduct')[0].reset();
+        });
+    }
 
-function makeApiCall(url, data, method) {
-    method = method || 'POST';
-    return $.ajax({
-        type: method,
-        url: url,
-        data: data,
-        dataType: 'json',
-        encode: true
-    })
-}
+    function setSalesReportsBehavior() {
+        //display data grid behavior
+        makeApiCall('/api/sales/potato', {}, 'GET')
+            .done(function (data) {
+                let gridHtml = generateGrid(data);
 
-function generateGrid(data) {
-    responseData = data;
+                $('#saleReports').html(gridHtml);
+                $('#myTable').DataTable();
+            });
+    }
 
-    let html = '<table id="myTable" class="table table-striped table-bordered productTableGrid" cellspacing="0"" width="100%"">' +
-        '<thead>' +
-        generateHeaderRecursive(data.column) +
-        '</thead><tbody>' +
-        generateDataRows(data) +
-        '</tbody></table>';
+    function showToast(headerTxt, bodyTxt) {
+        $('.toast-body').text(bodyTxt);
+        $('.toast-header').text(headerTxt);
+        $('.toast').toast({delay: 5000});
+        $('.toast').toast('show');
+    }
 
-    return html;
-}
+    function makeApiCall(url, data, method) {
+        method = method || 'POST';
+        return $.ajax({
+            type: method,
+            url: url,
+            data: data,
+            dataType: 'json',
+            encode: true
+        })
+    }
 
-function generateHeaderRecursive(columns) {
-    let result = composeHeaderRecursive(columns);
-    return (result.length == 2) ? `<tr>${result[0]}</tr><tr>${result[1]}</tr>` : `<tr>${result[0]}</tr>`;
-}
+    function generateGrid(data) {
+        responseData = data;
 
-function composeHeaderRecursive(columns, isChild) {
-    let htmlParentRow = '';
-    let htmlChildRow = '';
+        let html = '<table id="myTable" class="table table-striped table-bordered productTableGrid" cellspacing="0"" width="100%"">' +
+            '<thead>' +
+            generateHeaderRecursive(data.column) +
+            '</thead><tbody>' +
+            generateDataRows(data) +
+            '</tbody></table>';
 
-    columns.forEach(function (element) {
+        return html;
+    }
 
-        if (element['subHeaders']) {
-            htmlParentRow += '<th class="parentWithSubHeaders" colspan="' + element['subHeaders'].length + '">';
-            htmlParentRow += element.header;
-            htmlParentRow += '</th>';
+    function generateHeaderRecursive(columns) {
+        let result = composeHeaderRecursive(columns);
+        return (result.length == 2) ? `<tr>${result[0]}</tr><tr>${result[1]}</tr>` : `<tr>${result[0]}</tr>`;
+    }
 
-            htmlChildRow += composeHeaderRecursive(element['subHeaders'], true)[1];
-        } else {
-            if (isChild) {
-                htmlChildRow += '<th class="childElHeadCell">';
-                htmlChildRow += element.header;
-                htmlChildRow += '</th>';
+    function composeHeaderRecursive(columns, isChild) {
+        let htmlParentRow = '';
+        let htmlChildRow = '';
 
-            } else {
-                htmlParentRow += '<th class="parentWithoutSubHeaders" rowspan="2">';
+        columns.forEach(function (element) {
+
+            if (element['subHeaders']) {
+                htmlParentRow += '<th class="parentWithSubHeaders" colspan="' + element['subHeaders'].length + '">';
                 htmlParentRow += element.header;
                 htmlParentRow += '</th>';
-            }
 
-            if (element.field) {
-                leaves.push(element.field);
+                htmlChildRow += composeHeaderRecursive(element['subHeaders'], true)[1];
             } else {
-                leaves.push(element.header);
+                if (isChild) {
+                    htmlChildRow += '<th class="childElHeadCell">';
+                    htmlChildRow += element.header;
+                    htmlChildRow += '</th>';
+
+                } else {
+                    htmlParentRow += '<th class="parentWithoutSubHeaders" rowspan="2">';
+                    htmlParentRow += element.header;
+                    htmlParentRow += '</th>';
+                }
+
+                if (element.field) {
+                    leaves.push(element.field);
+                } else {
+                    leaves.push(element.header);
+                }
             }
-        }
-    });
+        });
 
-    return [htmlParentRow, htmlChildRow];
-}
+        return [htmlParentRow, htmlChildRow];
+    }
 
-function generateDataRows(data) {
-    let html = '';
+    function generateDataRows(data) {
+        let html = '';
 
-    data.data.forEach((row) => {
-        row['Total sales'] = row.salesQ1 + row.salesQ2 + row.salesQ3 + row.salesQ4;
-        html += generateOneRow(row);
-    });
-    return html;
-}
+        data.data.forEach((row) => {
+            row['Total sales'] = row.salesQ1 + row.salesQ2 + row.salesQ3 + row.salesQ4;
+            html += generateOneRow(row);
+        });
+        return html;
+    }
 
-function generateOneRow(row) {
-    let html = '<tr>';
-    leaves.forEach(el => {
-        html += '<td>';
-        html += row[el];
-        html += '</td>';
-    });
+    function generateOneRow(row) {
+        let html = '<tr>';
+        leaves.forEach(el => {
+            html += '<td>';
+            html += row[el];
+            html += '</td>';
+        });
 
-    html += '</tr>';
-    return html;
-}
+        html += '</tr>';
+        return html;
+    }
 
+    //export module utility functions
+    return {
+        setSalesReportsBehavior: setSalesReportsBehavior,
+        setAddNewProductBehavior: setAddNewProductBehavior
+    };
 
-
-
-
+})();
